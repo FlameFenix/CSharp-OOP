18152b45c1edae81b75fbc481cb773a042bea5ab
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-23 22:10:18 +0200

    Lab

---
+++ Inheritance - Lab/RandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        Random rnd = new Random();
+
+        public string RandomString()
+        {
+            int index = rnd.Next(0, this.Count);
+            string randomString = this[index];
+
+            this.Remove(randomString);
+
+            return randomString;
+        }
+    }
+}
---
+++ Inheritance - Lab/RandomList/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomlist = new RandomList();
+
+            randomlist.Add("pesho");
+            randomlist.Add("ivan");
+            randomlist.Add("stoqn");
+
+            randomlist.RandomString();
+
+            foreach (var item in randomlist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
