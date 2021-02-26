be930bb19e6bb291ac71f59245a223ff5bf9ba7f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-26 20:59:17 +0200

    Lab

---
+++ Encapsulation - Lab/Persons/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName { 
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string LastName {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public int Age {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
