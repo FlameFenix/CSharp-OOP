15dcc935349465c7498cde1a464b70d85facdb2e
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-01 21:18:17 +0200

    Exercises

---
+++ Encapsulation - Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+            }
+        }
+        public decimal Money
+        {
+            get => this.money;
+            private set
+            {
+                if (value >= 0)
+                {
+                    this.money = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+            }
+        }
+
+        public int Count 
+        {
+            get => this.products.Count;
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void DecreaseMoney(Product product)
+        {
+            this.money -= product.Cost;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.name} - {string.Join(", ", products)}";
+        }
+    }
+}
---
+++ Encapsulation - Exercise/ShoppingSpree/StartUp.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] players = Console.ReadLine()
+                                      .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                                      .ToArray();
+
+            List<Person> listPlayers = new List<Person>();
+            List<Product> listProducts = new List<Product>();
+
+            bool isEverythingValid = true;
+            try
+            {
+                for (int i = 0; i < players.Length; i++)
+                {
+                    string[] playerInfo = players[i].Split("=", StringSplitOptions.RemoveEmptyEntries)
+                                                .ToArray();
+                    string playerName = playerInfo[0];
+                    decimal playersMoney = decimal.Parse(playerInfo[1]);
+
+                    Person currentPerson = new Person(playerName, playersMoney);
+
+                    listPlayers.Add(currentPerson);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            
+
+            string[] products = Console.ReadLine()
+                                  .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                                  .ToArray();
+            try
+            {
+                for (int i = 0; i < products.Length; i++)
+                {
+                    string[] productInfo = products[i].Split("=", StringSplitOptions.RemoveEmptyEntries)
+                                                                          .ToArray();
+                    string productName = productInfo[0];
+                    decimal productPrice = decimal.Parse(productInfo[1]);
+
+                    Product currentProduct = new Product(productName, productPrice);
+
+                    listProducts.Add(currentProduct);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            
+
+
+            if (isEverythingValid)
+            {
+                string command = string.Empty;
+
+                while ((command = Console.ReadLine()) != "END")
+                {
+                    string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                              .ToArray();
+
+                    string personName = cmdArgs[0];
+                    string productName = cmdArgs[1];
+
+                    Person currentPerson = listPlayers.Find(x => x.Name == personName);
+
+                    Product currentProduct = listProducts.Find(x => x.Name == productName);
+
+                    if (currentPerson.Money >= currentProduct.Cost && listPlayers.Contains(currentPerson) && listProducts.Contains(currentProduct))
+                    {
+                        currentPerson.AddProduct(currentProduct);
+                        currentPerson.DecreaseMoney(currentProduct);
+                        Console.WriteLine($"{currentPerson.Name} bought {currentProduct.Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{currentPerson.Name} can't afford {currentProduct.Name}");
+                    }
+
+                }
+            }
+
+            if (isEverythingValid)
+            {
+                foreach (var item in listPlayers)
+                {
+                    if (item.Count == 0)
+                    {
+                        Console.WriteLine($"{item.Name} - Nothing bought");
+                    }
+                    else
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+
+        }
+    }
+}
