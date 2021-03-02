557bc0bac702712adefa6506bc62115951f63afb
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-02 21:38:39 +0200

    Lab

---
+++ Interfaces and Abstraction - Lab/Shapes/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public int Radius 
+        {
+            get => this.radius;
+            private set
+            {
+                this.radius = value;
+            }
+        }
+
+        public void Draw()
+        {
+            double rIn = this.Radius - 0.4;
+            double rOut = this.Radius + 0.4;
+
+            for (double rows = this.Radius; rows >= -this.Radius ; --rows)
+            {
+                for (double cols = -this.Radius; cols < rOut; cols += 0.5)
+                {
+                    double value = rows * rows + cols * cols;
+                    if(value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Interfaces and Abstraction - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width 
+        {
+            get => this.width;
+            private set
+            {
+                this.width = value;
+            }
+        }
+
+        public int Height
+        {
+            get => this.height;
+            private set
+            {
+                this.height = value;
+            }
+        }
+
+        public void Draw()
+        {
+            for (int rows = 0; rows < this.Width; rows++)
+            {
+                for (int cols = 0; cols < this.Height; cols++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
