e454dce76c347b635cfdb5e1e74a88c91653ee67
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-05 23:45:06 +0200

    Lab

---
+++ Polymorphism - Lab/Shapes/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(5,6);
+            Console.WriteLine(rectangle.Draw());
+        }
+    }
+}
