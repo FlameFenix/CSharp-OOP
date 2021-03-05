6cb805a2d8692730feb7f230ea2b31844e79f665
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-05 23:06:19 +0200

    Lab

---
+++ Polymorphism - Lab/Animals/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavoriteFood = favouriteFood;
+        }
+
+        public string Name { get => this.name; private set { this.name = value; } }
+        public string FavoriteFood { get => this.favouriteFood; private set {this.favouriteFood = value; } }
+
+        public virtual string ExplainSelf()
+        {
+            return string.Empty;
+        }
+    }
+}
