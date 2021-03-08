7f1d8b36f19067560844f5aff75a3d8d4c732f9a
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-08 23:55:40 +0200

    In Progress

---
+++ Interfaces and Abstraction - Exercise/MilitaryElite/Models/LieutenantGeneral.cs
@@ -0,0 +1,31 @@
+﻿using MilitaryElite.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class LieutenantGeneral : ILieutenantGeneral
+    {
+        
+
+        public LieutenantGeneral(string id, string firstName, string lastName, decimal salary)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Salary = salary;
+            this.privates = new List<IPrivate>();
+        }
+
+        public List<IPrivate> privates { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+    }
+}
---
+++ Interfaces and Abstraction - Exercise/MilitaryElite/Models/Private.cs
@@ -0,0 +1,26 @@
+﻿using MilitaryElite.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Private : IPrivate
+    {
+        public Private(string id, string firstName, string lastName, decimal salary)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Salary = salary;
+        }
+
+        public decimal Salary { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+    }
+}
