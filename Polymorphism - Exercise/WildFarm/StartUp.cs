369498ff0a37b7fd7f1cc57de79357629edb8235
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-11 21:51:11 +0200

    Edit

--- Polymorphism - Exercise/WildFarm/StartUp.cs
+++ Polymorphism - Exercise/WildFarm/StartUp.cs
@@ -27,51 +27,43 @@ namespace WildFarm
                     string name = cmdArgs[1];
                     double weight = double.Parse(cmdArgs[2]);
 
+                    Animal animal = null;
+
                     if (type == nameof(Animals.Birds.Hen))
                     {
                         double wingSize = double.Parse(cmdArgs[3]);
-
-                        Animal animal = new Animals.Birds.Hen(name, weight, wingSize);
-                        animals.Add(animal);
-                        animal.Sound();
+                        animal = new Animals.Birds.Hen(name, weight, wingSize); 
                     }
                     else if (type == nameof(Animals.Birds.Owl))
                     {
                         double wingSize = double.Parse(cmdArgs[3]);
-                        Animal animal = new Animals.Birds.Owl(name, weight, wingSize);
-                        animals.Add(animal);
-                        animal.Sound();
+                        animal = new Animals.Birds.Owl(name, weight, wingSize);
                     }
                     else if (type == nameof(Animals.Mammals.Cat))
                     {
                         string region = cmdArgs[3];
                         string breed = cmdArgs[4];
-                        Animal animal = new Animals.Mammals.Cat(name, weight, region, breed);
-                        animals.Add(animal);
-                        animal.Sound();
+                        animal = new Animals.Mammals.Cat(name, weight, region, breed);
                     }
                     else if (type == nameof(Animals.Mammals.Dog))
                     {
                         string region = cmdArgs[3];
-                        Animal animal = new Animals.Mammals.Dog(name, weight, region);
-                        animals.Add(animal);
-                        animal.Sound();
+                        animal = new Animals.Mammals.Dog(name, weight, region);
                     }
                     else if (type == nameof(Animals.Mammals.Mouse))
                     {
                         string region = cmdArgs[3];
-                        Animal animal = new Animals.Mammals.Mouse(name, weight, region);
-                        animals.Add(animal);
-                        animal.Sound();
+                        animal = new Animals.Mammals.Mouse(name, weight, region);
                     }
                     else if (type == nameof(Animals.Mammals.Tiger))
                     {
                         string region = cmdArgs[3];
                         string breed = cmdArgs[4];
-                        Animal animal = new Animals.Mammals.Tiger(name, weight, region, breed);
-                        animals.Add(animal);
-                        animal.Sound();
+                        animal = new Animals.Mammals.Tiger(name, weight, region, breed);
                     }
+
+                    animals.Add(animal);
+                    animal.Sound();
 
                     countOfAnimals++;
 
@@ -83,26 +75,27 @@ namespace WildFarm
                                               .Split(" ", StringSplitOptions.RemoveEmptyEntries);
                     string foodType = cmdArgs[0];
                     int foodQuantity = int.Parse(cmdArgs[1]);
+
+                    Food food = null;
+
                     if(foodType == nameof(Foods.Fruit))
                     {
-                        Food food = new Foods.Fruit(foodQuantity);
-                        animals[countOfAnimals - 1].Eat(food);
+                        food = new Foods.Fruit(foodQuantity);
                     }
                     else if(foodType == nameof(Foods.Meat))
                     {
-                        Food food = new Foods.Meat(foodQuantity);
-                        animals[countOfAnimals - 1].Eat(food);
+                        food = new Foods.Meat(foodQuantity);
                     }
                     else if (foodType == nameof(Foods.Seeds))
                     {
-                        Food food = new Foods.Seeds(foodQuantity);
-                        animals[countOfAnimals - 1].Eat(food);
+                        food = new Foods.Seeds(foodQuantity);
                     }
                     else if (foodType == nameof(Foods.Vegetable))
                     {
-                        Food food = new Foods.Vegetable(foodQuantity);
-                        animals[countOfAnimals - 1].Eat(food);
+                        food = new Foods.Vegetable(foodQuantity);
                     }
+
+                    animals[countOfAnimals - 1].Eat(food);
 
                     index++;
                 }
