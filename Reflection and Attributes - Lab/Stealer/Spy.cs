b77c7f7386886678cba3f5bac09f72dab153fb30
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-16 22:45:51 +0200

    Lab

---
+++ Reflection and Attributes - Lab/Stealer/Spy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            FieldInfo[] fieldInfos = classType.GetFields( BindingFlags.Instance
+                                                    | BindingFlags.Static 
+                                                    | BindingFlags.Public 
+                                                    | BindingFlags.NonPublic);
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Class under investigation: {Type.GetType(investigatedClass)}");
+
+            foreach (var field in fieldInfos.Where(x => requestedFields.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/Stealer/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Stealer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            string stealInfo = spy.StealFieldInfo("Stealer.Hacker", new string[] { "username", "password" });
+
+            Console.WriteLine(stealInfo);
+        }
+    }
+}
