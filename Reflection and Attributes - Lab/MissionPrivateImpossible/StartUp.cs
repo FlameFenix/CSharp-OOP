34e62c752c2873ce9ae9581b494e0a32a962227d
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-16 23:22:53 +0200

    Lab

---
+++ Reflection and Attributes - Lab/MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MissionPrivateImpossible
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            string stealInfo = spy.RevealPrivateMethods("MissionPrivateImpossible.Hacker");
+
+            Console.WriteLine(stealInfo);
+        }
+    }
+}
