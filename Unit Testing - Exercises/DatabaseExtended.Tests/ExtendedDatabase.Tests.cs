dc91e32271ec6a2e36c2408270ea6c73504e960d
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-23 20:58:06 +0200

    DatabaseExtended 92/100

--- Unit Testing - Exercises/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
+++ Unit Testing - Exercises/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -9,7 +10,7 @@ namespace Tests
         private Person person;
 
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
             database = new ExtendedDatabase();
         }
@@ -124,6 +125,57 @@ namespace Tests
             person = new Person(id, "stoqn");
             database.Add(person);
             Assert.That(database.FindById(id), Is.EqualTo(person));
+        }
+
+        [Test]
+        public void AddingPersonWithConstructor()
+        {
+            ExtendedDatabase database = new ExtendedDatabase(new Person(10, "Pesho"), new Person(11, "Stoqn"));
+            Assert.That(database.Count == 2);
+        }
+
+        [Test]
+        public void ValidatePersonCountIsSixteenOverConstructor()
+        {
+            Assert.Throws<ArgumentException>(() => database = new ExtendedDatabase(new Person(1, "Pesho"),
+                                                              new Person(2, "Pesho"),
+                                                              new Person(3, "Pesho"),
+                                                              new Person(4, "Pesho"),
+                                                              new Person(5, "Pesho"),
+                                                              new Person(6, "Pesho"),
+                                                              new Person(7, "Pesho"),
+                                                              new Person(8, "Pesho"),
+                                                              new Person(9, "Pesho"),
+                                                              new Person(10, "Pesho"),
+                                                              new Person(11, "Pesho"),
+                                                              new Person(12, "Pesho"),
+                                                              new Person(13, "Pesho"),
+                                                              new Person(14, "Pesho"),
+                                                              new Person(15, "Pesho"),
+                                                              new Person(16, "Pesho"),
+                                                              new Person(17, "Pesho")));
+        }
+
+        [Test]
+        public void WhenAddingPersonsWithSameNamesByConstructor()
+        {
+            Assert.Throws<InvalidOperationException>(() => database = new ExtendedDatabase(new Person(5, "Pesho"), new Person(6, "Pesho")));
+        }
+
+        [Test]
+        public void WhenAddingPersonsWithSameIDByConstructor()
+        {
+            Assert.Throws<InvalidOperationException>(() => database = new ExtendedDatabase(new Person(5, "Pesho"), new Person(5, "Stoqn")));
+        }
+
+        [Test]
+        public void ValidateAddPositionOfElements()
+        {
+            database.Add(new Person(1, "Pesho"));
+            int indexOfPesho = database.Count;
+            database.Add(new Person(2, "Stoqn"));
+            int indexOfStoqn = database.Count;
+            Assert.That(indexOfPesho == indexOfStoqn - 1);
         }
     }
 }
